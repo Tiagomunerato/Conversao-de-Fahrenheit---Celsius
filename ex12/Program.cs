2f87fd1f01cc52078e3561a0b7fdc7c04c5e3f92
Tiagomunerato <tiagomunerato@gmail.com>
2019-10-05 09:57:27 -0300

    Add project files.

---
+++ ex12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, c;
+
+            Console.Write("Informe a Temperatura de ºF => ");
+            f = double.Parse(Console.ReadLine());
+
+            c = ((f - 32) * 5) / 9;
+
+            Console.WriteLine("{0:N} ºF correspondem a {1:N} ºC", f, c);
+            //Console.WriteLine( f + " ºF correspndem a " + c + " ºC");
+            Console.ReadKey();
+        }
+    }
+}
